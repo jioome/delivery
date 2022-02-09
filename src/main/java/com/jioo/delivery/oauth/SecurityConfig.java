06ac638605728aacb6f63373a66bc16a0e24fce5
minkukjo <strike0115@naver.com>
2022-02-09 19:37:07 +0900

    yaml Path 수정

--- src/main/java/com/jioo/delivery/oauth/SecurityConfig.java
+++ src/main/java/com/jioo/delivery/oauth/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @RequiredArgsConstructor
 @EnableWebSecurity // Spring Security 설정 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
@@ -30,7 +30,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .userInfoEndpoint() // oauth2 로그인 성공 후 가져올 때의 설정들
                 // 소셜로그인 성공 시 후속 조치를 진행할 UserService 인터페이스 구현체 등록
                 .userService(customOAuth2UserService); // 리소스 서버에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능 명시
-
-        super.configure(http);
     }
 }
