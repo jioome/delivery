3ee72a9b364d4170ec09931c87b4ee51dc7aa493
Jioo Jung <64303211+jioome@users.noreply.github.com>
2022-02-22 19:49:11 +0900

    kakao login/getInfo

---
+++ src/main/java/com/jioo/delivery/controller/RootController.java
@@ -0,0 +1,15 @@
+package com.jioo.delivery.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home(){
+        return "delivery";
+    }
+}
