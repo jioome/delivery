d0b216687ca0f4e0ba2ecaf36164ec42a71d0045
minkukjo <strike0115@naver.com>
2022-05-04 21:00:01 +0900

    레디스 캐시 설정 추가

--- src/main/java/com/jioo/delivery/oauth/User.java
+++ src/main/java/com/jioo/delivery/oauth/User.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
+
 import java.util.Collection;
 
 @Data
@@ -17,27 +18,26 @@ public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
-    private String email ;
+    private String email;
 
     @Column(name = "user_id")
-    private int userId ;
+    private int userId;
 
-//    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private Role role = Role.USER;
+    private Role role;
 
     private String picture;
 
 
-
     @Builder
-    public User(int id,String name, String email, String picture ,Role role,  int userId){
+    public User(Long id, String name, String email, String picture, Role role, int userId) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -47,7 +47,7 @@ public class User extends BaseTimeEntity {
     }
 
     @Builder
-    public User(int id, String name, String email, String picture,int userId) {
+    public User(Long id, String name, String email, String picture, int userId) {
         super();
     }
 
@@ -56,14 +56,13 @@ public class User extends BaseTimeEntity {
     }
 
 
-    public User update(String name){
+    public User update(String name) {
         this.name = name;
         return this;
     }
 
 
-
-    public String getRoleKey(){
+    public String getRoleKey() {
         return this.role.getKey();
     }
 }
--- src/main/java/com/jioo/delivery/service/UserService.java
+++ src/main/java/com/jioo/delivery/service/UserService.java
@@ -5,6 +5,8 @@ import com.jioo.delivery.oauth.Role;
 import com.jioo.delivery.oauth.User;
 import com.jioo.delivery.oauth.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -18,12 +20,13 @@ public class UserService {
 
     private final List<User> users = new ArrayList<>();
 
-// create 없이 delete update read 만 있어도 됨
+    // create 없이 delete update read 만 있어도 됨
     public User create(UserRequest userParameter) {
-        final User user = new User( userParameter.getId(),userParameter.getName(),userParameter.getEmail(),userParameter.getPicture(),userParameter.getRole(),userParameter.getUserId());
+        final User user = new User(userParameter.getId(), userParameter.getName(), userParameter.getEmail(), userParameter.getPicture(), userParameter.getRole(), userParameter.getUserId());
         return userRepository.save(user);
     }
 
+    @Cacheable(cacheNames = "user", key = "#id")
     public User read(Long id) {
         return userRepository.findById(id).orElseThrow(NullPointerException::new);
     }
